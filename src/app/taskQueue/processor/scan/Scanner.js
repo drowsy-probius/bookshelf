e85f1abf26c2941d9a505147e882164397c96700
k123s456h <k123s456h@gmail.com>
2021-02-18 14:49:30 +0900

    Scanner constructor error handling

--- src/app/taskQueue/processor/scan/Scanner.js
+++ src/app/taskQueue/processor/scan/Scanner.js
@@ -1,12 +1,31 @@
 const path = require('path');
+const fs = require('fs');
 
 const {logger} = require('../../../log');
 
 const {scanners} = require('../../../plugins/scanner');
 
+/**
+ * new Scanner(validDirectoryPath, validScannerName)
+ */
 class Scanner {
   constructor(folderPath, scannerName)
   {
+    if(fs.existsSync(path.resolve(folderPath)) === false)
+    {
+      throw new Error('[' + path.resolve(folderPath) + '] does not exists!');
+    }
+
+    if(fs.statSync(path.resolve(folderPath)).isDirectory() === false)
+    {
+      throw new Error('[' + path.resolve(folderPath) + '] is not a directory!');
+    }
+
+    if(scanners[scannerName] === undefined)
+    {
+      throw new Error('[' + scannerName + '] scanner does not exist!')
+    }
+
     this.root = path.resolve(folderPath);
     this.scan = scanners[scannerName];
     this.logger = logger;
